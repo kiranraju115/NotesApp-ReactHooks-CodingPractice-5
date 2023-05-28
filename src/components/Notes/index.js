aed4acbfec6e816c0208962c761c681d29792ee0
kiranraju <kiranraju115@gmail.com>
2023-05-28 12:23:30 +0000

    first commit

---
+++ src/components/Notes/index.js
@@ -0,0 +1,92 @@
+// Write your code here
+import {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'
+
+import NoteItem from '../NoteItem'
+
+import {
+  MainContainer,
+  NotesContainer,
+  Heading,
+  Form,
+  TitleInput,
+  NoteTextArea,
+  AddButton,
+  EmptyNotesViewContainer,
+  Image,
+  EmptyNotesHeading,
+  Description,
+  NotesList,
+} from './styledComponents'
+
+const Notes = () => {
+  const [title, setTitle] = useState('')
+  const [note, setNote] = useState('')
+  const [notesList, setNotesList] = useState([])
+
+  const onChangeTitle = event => {
+    setTitle(event.target.value)
+  }
+
+  const onChangeNote = event => {
+    setNote(event.target.value)
+  }
+
+  const onAddNote = event => {
+    event.preventDefault()
+    const newNote = {
+      id: uuidv4(),
+      title,
+      note,
+    }
+
+    setNotesList(prevNotesList => [...prevNotesList, newNote])
+    setNote('')
+    setTitle('')
+  }
+
+  const renderNotes = () => (
+    <NotesList>
+      {notesList.map(eachNote => (
+        <NoteItem key={eachNote.id} noteDetails={eachNote} />
+      ))}
+    </NotesList>
+  )
+
+  const renderEmptyNotesView = () => (
+    <EmptyNotesViewContainer>
+      <Image
+        src="https://assets.ccbp.in/frontend/hooks/empty-notes-img.png"
+        alt="notes empty"
+      />
+      <EmptyNotesHeading>No Notes Yet</EmptyNotesHeading>
+      <Description>Notes you add will appear here</Description>
+    </EmptyNotesViewContainer>
+  )
+
+  return (
+    <MainContainer>
+      <NotesContainer>
+        <Heading>Notes</Heading>
+        <Form onSubmit={onAddNote}>
+          <TitleInput
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={onChangeTitle}
+          />
+          <NoteTextArea
+            type="text"
+            placeholder="Take a Note..."
+            rows="3"
+            value={note}
+            onChange={onChangeNote}
+          />
+          <AddButton type="submit">Add</AddButton>
+        </Form>
+        {notesList.length === 0 ? renderEmptyNotesView() : renderNotes()}
+      </NotesContainer>
+    </MainContainer>
+  )
+}
+export default Notes
